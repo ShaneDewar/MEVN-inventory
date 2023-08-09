bb1a68f554d26169984f318f6b3ea6fca70aa9ee
Shane Dewar <ShaneB.Dewar@gmail.com>
2023-08-08 22:46:21 -0400

    Experimenting with routes, fetch and axios (testing)

--- frontend/src/components/Server.vue
+++ frontend/src/components/Server.vue
@@ -11,13 +11,18 @@ import { defineComponent } from 'vue';
 
 export default defineComponent({
     name: "Server",
-    data() { return { response: null, }; },
+    data() {
+        return {
+            response: null,
+        };
+    },
     methods: {
         async getServerRes() {
+            // const response = await ObjectService.getAll();
             const response = await fetch("http://localhost:3033/server");
             const data = await response.json();
             this.response = data.message;
-        },
+        }
     },
 });
 </script>
