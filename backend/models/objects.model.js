248f4668fe336291a8209e4ba2469d910687dd90
Shane Dewar <ShaneB.Dewar@gmail.com>
2023-08-14 00:47:20 -0400

    Setup mongoose and mongo to store objects

---
+++ backend/models/objects.model.js
@@ -0,0 +1,19 @@
+module.exports = mongoose => {
+    let schema = mongoose.Schema(
+        {
+            object: String,
+            message: String
+        },
+        { timestamps: true }
+    );
+
+    schema.method("toJSON", function () {
+        const { __v, _id, ...object } = this.toObject();
+        object.id = _id;
+        return object;
+
+    });
+
+    const Object = mongoose.module("object", schema);
+    return Object;
+};
