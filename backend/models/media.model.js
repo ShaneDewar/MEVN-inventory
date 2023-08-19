104497b808a5a599f831473e867011bc99e642fa
Shane Dewar <ShaneB.Dewar@gmail.com>
2023-08-18 21:22:17 -0400

    Add useful data modelling

--- backend/models/media.model.js
+++ backend/models/media.model.js
@@ -1,19 +1,28 @@
-module.exports = mongoose => {
-    let schema = mongoose.Schema(
-        {
-            media: String,
-            summary: String
-        },
-        { timestamps: true }
-    );
+module.exports = (mongoose) => {
+  let schema = mongoose.Schema(
+    {
+      title: String,
+      authors: [String],
+      format: String,
+      publish_date: Date,
+      date_added: { type: Date, default: Date.now },
+      genres: [String],
+      have_used: Boolean,
+      keywords: [String],
+      languages: [String],
+      isbn: String,
+      size: String,
+      notes: [String],
+    },
+    { timestamps: true },
+  );
 
-    schema.method("toJSON", function () {
-        const { __v, _id, ...object } = this.toObject();
-        object.id = _id;
-        return object;
+  schema.method("toJSON", function () {
+    const { __v, _id, ...object } = this.toObject();
+    object.id = _id;
+    return object;
+  });
 
-    });
-
-    const Media = mongoose.model("media", schema);
-    return Media;
+  const Media = mongoose.model("media", schema);
+  return Media;
 };
