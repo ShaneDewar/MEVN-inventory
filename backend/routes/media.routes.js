51dc35577f493d2f5536c524290f1a84d0c22c66
Shane Dewar <ShaneB.Dewar@gmail.com>
2023-08-24 18:54:59 -0400

    Initial Create and Findall server routes

--- backend/routes/media.routes.js
+++ backend/routes/media.routes.js
@@ -1,1 +1,11 @@
-module.exports = (routes) => {};
+module.exports = (routes) => {
+  const media = require("../controllers/media.controller.js");
+
+  let router = require("express").Router();
+
+  // Create Media
+  router.post("/create", media.create);
+
+  // Find all Media
+  router.get("/findAll", media.findAll);
+};
