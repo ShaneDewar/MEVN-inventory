516bfc18d79f7c67056878f256b15a8159399bdb
Shane Dewar <ShaneB.Dewar@gmail.com>
2023-09-29 23:30:05 -0400

    docs: landing page/readme

--- frontend/src/components/LandingPage.vue
+++ frontend/src/components/LandingPage.vue
@@ -1,5 +1,9 @@
 <template>
   <h1>
-    This is the landing page. It will have a brief description of the project.
+    MEVN Library
   </h1>
+  This project is an example project representing a personal library. So you can add your media, search through your library, update and remove titles as well as track if you've read or listened to the media and add whatever notes you may like.
+  The library is a Vue 3 SPA that uses Vue + Vue Router + Vite + Axios for the front end, an Express + Node + Mongoose server, and Mongodb for the database.
+
+  
 </template>
